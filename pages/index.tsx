11c88c3fdd27bfa98ed2e71bf5b75ee689d8dff6
ItsTunggg <ttnhh9@umsystem.edu>
2023-06-24 11:26:43 +0700

    Title complete-ish, I resize some of it later

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,14 +10,15 @@ const Home: NextPage = () => {
                 <title>PickHacks 2024</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-
             <main className={styles.main}>
                 <p>Missouri S&T presents...ඞ</p>
-            <object type="image/svg+xml" data="line.svg" style={{position: "absolute", top: 0, right: 0, height: "100%", width: "110%", margin: 0, padding: 0}}></object>
+                <img src="images/Pickhacks2024.png" alt="Pickhacks2024" style={{position:"absolute",left:22,bottom:250,height:"30%",border:0}}></img>
+                <h1>coming soon</h1>
+                <object type="image/svg+xml" data="line.svg" style={{position: "absolute", top: 0, right: 0, height: "100%", width: "110%", margin: 0, padding: 0}}></object>
             </main>
+            
         </div>
     );
 };
-
 export default Home;
 
