6bca7626d216d3cc6da28d1392a1336cb0b594da
ezybg7 <ezybg7@umsystem.edu>
2023-06-26 14:42:41 -0500

    uhhhh cleaned some things up

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,24 +12,25 @@ const Home: NextPage = () => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main className={styles.main}>
-                <p>Missouri S&T presents...ඞ</p>
-                <img src="images/Pickhacks2024.png" alt="Pickhacks2024" style={{position:"absolute",left:22,bottom:250,height:"30%",border:0}}></img>
-                <h1>coming soon</h1>
-                <object type="image/svg+xml" data="line.svg" style={{position: "absolute", top: 0, right: 0, height: "100%", width: "110%", margin: 0, padding: 0}}></object>
-                <p>Missouri S&T presents...</p>
-                <form action="/api/Email">
-                    <label htmlFor="email">Email:</label>
-                    <input
-                        type="text"
-                        id="email"
-                        name="email"
-                        pattern="^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$"
-                        title="please submit a valid email address"
-                        required />
+                <div className='info-box'>
+                    <object type="image/svg+xml" data="pickhacks.png" style={{position: "absolute", top: 0, left: 0, height: "100%", width: "100%", margin: 0, padding: 0}}></object>
+                    <p>Missouri S&T presents...ඞ</p>
+                    <h1>PickHacks 2024</h1>
+                    <h2>coming soon...</h2>
+                    <form action="/api/Email">
+                        <label htmlFor="email">Email:</label>
+                        <input
+                            type="text"
+                            id="email"
+                            name="email"
+                            pattern="^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$"
+                            title="please submit a valid email address"
+                            required />
 
-                    <button type="submit">Submit</button>
-                </form>
-              <object type="image/svg+xml" data="line.svg" style={{position: "absolute", top: 0, right: 0, height: "100%", width: "110%", margin: 0, padding: 0}}></object>
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
+                <object type="image/svg+xml" data="line.svg" style={{position: "absolute", top: 0, right: 0, height: "100%", width: "110%", margin: 0, padding: 0}}></object>
             </main>
             
         </div>
