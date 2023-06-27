7f49d8b9646543b1e40724ff56007d14b2c8cb3f
ewbyf <107148516+ewbyf@users.noreply.github.com>
2023-06-26 22:17:01 -0500

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,26 +11,48 @@ const Home: NextPage = () => {
                 <title>PickHacks 2024</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+            <Image src="/images/pickhacks.png" width={65} height={65} alt="logo" className={styles.logo}/>
             <main className={styles.main}>
-                <div className='info-box'>
-                    <object type="image/svg+xml" data="pickhacks.png" style={{position: "absolute", top: 0, left: 0, height: "100%", width: "100%", margin: 0, padding: 0}}></object>
-                    <p>Missouri S&T presents...ඞ</p>
-                    <h1>PickHacks 2024</h1>
-                    <h2>coming soon...</h2>
-                    <form action="/api/Email">
-                        <label htmlFor="email">Email:</label>
-                        <input
-                            type="text"
-                            id="email"
-                            name="email"
-                            pattern="^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$"
-                            title="please submit a valid email address"
-                            required />
 
-                        <button type="submit">Submit</button>
-                    </form>
+                <div className={styles.info}>
+                    <div className={styles.textContainer}>
+                        <p className={styles.top}>Missouri S&T presents...</p>
+                        <p className={styles.title}>PickHacks 2024</p>
+                        <p className={styles.bottom}>coming soon...</p>
+                    </div>
+                    <div>
+                        <p className={styles.text}>Interested? Join our mailing list!</p>
+                        <form action="/api/Email">
+                            <label htmlFor="email">Email:</label>
+                            <input
+                                type="text"
+                                id="email"
+                                name="email"
+                                pattern="^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$"
+                                title="please submit a valid email address"
+                                required />
+
+                            <button type="submit">Submit</button>
+                        </form>
+                    </div>
+                    <div className={styles.socials}>
+                        <p>Check out our socials</p>
+                        <div className={styles.buttonContainer}>
+                            <div className={styles.socialImage}>
+                                <Image src="/images/discord.png" width={35} height={24} alt="logo"/>
+                            </div>
+                            <div className={styles.socialImage}>
+                                <Image src="/images/linkedin.png" width={35} height={33} alt="logo"/>
+                            </div>
+                            <div className={styles.socialImage}>
+                                <Image src="/images/instagram.png" width={34} height={34} alt="logo"/>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <object type="image/svg+xml" data="line.svg" style={{position: "absolute", top: 0, right: 0, height: "100%", width: "110%", margin: 0, padding: 0}}></object>
+                <div style={{width: "40%", height: "100%"}}>
+                    <object type="image/svg+xml" data="line.svg" style={{ position: "absolute", top: 0, right: 0, margin: 0, padding: 0, pointerEvents: 'none', width: "100em", overflow: "hidden"}}></object>
+                </div>
             </main>
             
         </div>
