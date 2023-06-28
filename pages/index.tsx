5e11d78eadd4a0d0655b07196ea5cea1f50f2822
Jason Xu <Xinwen31415@gmail.com>
2023-06-28 07:39:42 -0500

    fixed hydration issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,43 @@
+import axios from 'axios';
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { useState } from 'react';
 import { IoSendSharp } from 'react-icons/io5';
 import { MouseParallax } from 'react-just-parallax';
 import { useMediaQuery } from 'usehooks-ts';
 import styles from '../styles/Home.module.css';
-import { useState } from 'react';
-import axios from 'axios';
 
 const Home: NextPage = () => {
-	const matches = useMediaQuery('(max-width: 1440px)');
+	const matches = useMediaQuery('(max-width: 1440px)') || typeof window === 'undefined';
 	const [email, setEmail] = useState<string>('');
 	const [success, setSuccess] = useState<boolean>(false);
 	const [error, setError] = useState<boolean>(false);
 
-
 	const submitEmail = () => {
-		if (email.toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
-			axios.post('/api/Email', {
-				email
-			})
-			.then((resp) => {
-				setEmail('');
-				setError(false);
-				setSuccess(true);
-			})
-			.catch((err) => {
-				console.log(err)
-			})
-		}
-		else {
+		if (
+			email
+				.toLowerCase()
+				.match(
+					/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+				)
+		) {
+			axios
+				.post('/api/Email', {
+					email
+				})
+				.then((resp) => {
+					setEmail('');
+					setError(false);
+					setSuccess(true);
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		} else {
 			setError(true);
 			setSuccess(false);
 		}
-	}
+	};
 
 	return (
 		<div className={styles.container}>
@@ -52,14 +57,13 @@ const Home: NextPage = () => {
 					<div className={styles.mailing}>
 						<p className={styles.text}>Interested? Join our mailing list!</p>
 						<form>
-							<input
-								type="text"
-								placeholder="Enter email address"
-								value={email}
-								onChange={(e) => setEmail(e.target.value)}
-								required
-							/>
-							<button className={styles.submit} onClick={(e) => {e.preventDefault(); submitEmail()}}>
+							<input type="text" placeholder="Enter email address" value={email} onChange={(e) => setEmail(e.target.value)} required />
+							<button
+								className={styles.submit}
+								onClick={(e) => {
+									e.preventDefault();
+									submitEmail();
+								}}>
 								<IoSendSharp color="#0A4C72" />
 							</button>
 						</form>
@@ -89,7 +93,7 @@ const Home: NextPage = () => {
 								(elem.children[0] as any).style.overflow = 'visible';
 								(elem.children[0].children[0] as any).style.overflow = 'visible';
 							}
-					}}>
+						}}>
 						<MouseParallax strength={0.03} isAbsolutelyPositioned>
 							<iframe src="line.svg" height={1125} width={2350} className={styles.svg}></iframe>
 						</MouseParallax>
